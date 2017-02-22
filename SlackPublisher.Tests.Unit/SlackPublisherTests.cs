efb3442bcfa6797daf8ebca372b4070fe0a7782f
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-02-22 22:36:19 +0100

    Status filter introduced, with tests

---
+++ SlackPublisher.Tests.Unit/SlackPublisherTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using NUnit.Framework;
+using SlackPublisher.Messaging;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace SlackPublisher.Tests.Unit
+{
+    [TestFixture]
+    public class SlackPublisherTests
+    {
+        [Test]
+        public void WhenStatusConfiguredToBeConsumedThenSendsMessage()
+        {
+            var status = IntegrationStatus.Failure;
+
+            var mockResult = new Mock<IIntegrationResult>();
+            mockResult.SetupGet(m => m.Status).Returns(status);
+
+            var messageSender = new Mock<IMessageSender>();
+
+            var sut = this.GetSut(messageSender.Object);
+            sut.RawStatusesList = status.ToString();
+
+            sut.Run(mockResult.Object);
+
+            messageSender.Verify(m => m.Send(It.IsAny<string>(), It.IsAny<Payload>()));
+        }
+
+        [Test]
+        public void WhenStatusNotConfiguredToBeConsumedThenDoesntSendMessage()
+        {
+            var status = IntegrationStatus.Failure;
+
+            var mockResult = new Mock<IIntegrationResult>();
+            mockResult.SetupGet(m => m.Status).Returns(status);
+
+            var messageSender = new Mock<IMessageSender>(MockBehavior.Strict);
+
+            var sut = this.GetSut(messageSender.Object);
+            sut.RawStatusesList = "SomeOtherStatus";
+
+            sut.Run(mockResult.Object);            
+        }
+
+        private ThoughtWorks.CruiseControl.Core.Publishers.SlackPublisher GetSut(IMessageSender fakeMessageSender)
+        {
+            var messageSender = fakeMessageSender ?? new Mock<IMessageSender>().Object;
+
+            return new ThoughtWorks.CruiseControl.Core.Publishers.SlackPublisher(messageSender)
+            {
+                WebhookUrl = "some url"
+            };
+        }
+    }
+}
--- SlackPublisher/SlackPublisher.cs
+++ SlackPublisher/SlackPublisher.cs
@@ -1,5 +1,9 @@
 ﻿using Exortech.NetReflector;
 using SlackPublisher.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThoughtWorks.CruiseControl.Remote;
 
 namespace ThoughtWorks.CruiseControl.Core.Publishers
 {
@@ -16,11 +20,19 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
         }
 
         [ReflectorProperty("webhookUrl")]
-        public string WebhookUrl { get; set; }        
+        public string WebhookUrl { get; set; }
+
+        [ReflectorProperty("statuses")]
+        public string RawStatusesList { get; set; }
+
+        private IEnumerable<IntegrationStatus> StatusesToConsume => this.GetStatusesToConsume();        
 
         public void Run(IIntegrationResult result)
         {
             if (!this.IsConfigurationValid())
+                return;
+
+            if (!this.StatusesToConsume.Contains(result.Status))
                 return;
 
             var message = this.FormatText(result);
@@ -29,9 +41,25 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
             this.messageSender.Send(this.WebhookUrl, payload);
         }
 
+        private IEnumerable<IntegrationStatus> GetStatusesToConsume()
+        {
+            if (string.IsNullOrEmpty(this.RawStatusesList))                            
+                yield break;
+            
+            var splitStatuses = this.RawStatusesList.Split(';');
+
+            foreach (var rawStatus in splitStatuses)
+            {
+                if (Enum.IsDefined(typeof(IntegrationStatus), rawStatus))
+                {
+                    yield return (IntegrationStatus)Enum.Parse(typeof(IntegrationStatus), rawStatus);
+                }
+            }
+        }
+
         private bool IsConfigurationValid()
         {
-            return !string.IsNullOrEmpty(WebhookUrl);
+            return !string.IsNullOrEmpty(this.WebhookUrl);
         }
 
         private string FormatText(IIntegrationResult result)
